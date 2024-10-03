9329104b1274ef3043fcae4e93fa10556694a351
NEMYSESx <shakirraza4227@gmail.com>
2024-10-04 00:03:35 +0530

    fixed error

--- app/(dashboard)/[storeId]/(routes)/categories/_component/columns.tsx
+++ app/(dashboard)/[storeId]/(routes)/categories/_component/columns.tsx
@@ -11,14 +11,14 @@ import { CellAction } from "./cell-action";
 
 // This type is used to define the shape of our data.
 // You can use a Zod schema here if you want.
-export type LocalCategoryColumn = {
+export type CategoryColumn = {
   id: string;
   name: string;
   billboardLabel: string;
   createdAt: string;
 };
 
-export const columns: ColumnDef<LocalCategoryColumn>[] = [
+export const columns: ColumnDef<CategoryColumn>[] = [
   {
     accessorKey: "name",
     header: "Name",
